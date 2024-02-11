bebfc6ad905571180cb02a79e4d03ec5a23e281f
Praveen <praveennooka123@gmail.com>
2024-02-11 17:09:59 +0530

    changed css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,13 @@ import EmailIcon from "@material-ui/icons/Email";
 import GithubIcon from "@material-ui/icons/GitHub";
 import OpenInNewIcon from '@material-ui/icons/OpenInNew'
 import "../styles/Home.css"
+import { StoreMallDirectory } from '@material-ui/icons';
 
 function Home() {
   return (
     <div className="home">
       <div className='about'>
-        <h2>Hi, My Name is Praveen !</h2>
+        <h2>Hi, I'm Praveen!</h2>
         <div className='prompt'>
           <p>
             A Competitive Programmer
@@ -24,10 +25,10 @@ function Home() {
         </div>
         <div className='myprofiles'>
           <a href="https://www.linkedin.com/in/praveennooka/" target="_blank" rel="noreferrer">
-              <LinkedInIcon />
+            <LinkedInIcon />
           </a>
           <a href="mailto:praveennooka123@gmail.com">
-              <EmailIcon />
+            <EmailIcon />
           </a>
           <a href="https://github.com/nova0077" target="_blank" rel="noreferrer">
             <GithubIcon />
@@ -35,66 +36,79 @@ function Home() {
         </div>
       </div>
       <div className='skillsContainer'>
-        <h1 className='heading'>SKILLS</h1>
+        <h1 className='heading'>About me</h1>
         <div className='skills'>
-          <h1> Problem Solving </h1>
-          <ul className='list'>
-            <li className='item'>
-              <h2> Competitive Programming</h2>
-              <span>
-                Codeforces: Specialist,<br/> 
-                Codechef : 4 star<br/>
-                Leetcode: 1822
-              </span>
-            </li>
-            <li className='item'>
-              <h2>DSA</h2>
-              <span>
-                Leetcode: 422 problems <br/>
-                GFG: 315 problems
-              </span>
-            </li>
-            <li className='item'>
-              <h2>Coding Profiles</h2>
-              <span>
-                CodeForces : 
-                <a href="https://codeforces.com/profile/novaa" target='_blank' rel="noreferrer" style={{all:"unset", color: "white"}}>
-                   <span style={{color:"red"}}> novaa</span> {<OpenInNewIcon />} 
-                </a> <br/>
-                CodeChef : 
-                <a href="https://www.codechef.com/users/novaa" target='_blank' rel="noreferrer" style={{all:"unset", color: "white"}}>
-                   <span style={{color:"red"}}> novaa</span> {<OpenInNewIcon />} 
-                </a> <br/>
-                LeetCode: 
-                <a href="https://leetcode.com/novaa/" target='_blank' rel="noreferrer" style={{all:"unset", color: "white"}}>
-                   <span style={{color:"red"}}> novaa</span> {<OpenInNewIcon />} 
-                </a> <br/>
-              </span>
-            </li>
-          </ul>
-        </div>
-        <div className='skills'>
-          <h1> Development</h1>
-          <ul className='list'>
-            <li className='item'>
-              <h2> Front-End</h2>
-              <span>
-                ReactJS, Redux, HTML, CSS, Bootstrap, MaterialUI, TailwindCSS
-              </span>
-            </li>
-            <li className='item'>
-              <h2>Back-End</h2>
-              <span>
-                NodeJS, ExpressJS, Socket.IO, Flask, MySQL, MongoDB
-              </span>
-            </li>
-            <li className='item'>
-              <h2>Languages</h2>
-              <span>
-                JavaScript, C++, Python, TypeScript
-              </span>
-            </li>
-          </ul>
+          <div id="skill1">
+            <h1> Problem Solving </h1>
+            <table>
+              <tr>
+                <th>Platform</th>
+                <th>Handle</th>
+                <th>Rank</th>
+                <th>Max Rating</th>
+                <th>Problems Solved</th>
+              </tr>
+              <tr>
+                <td>Codeforces</td>
+                <td>
+                  <a href="https://codeforces.com/profile/novaa" target='_blank' rel="noreferrer" style={{ all: "unset", color: "white" }}>
+                    <span style={{ color: "red" }}> novaa</span> {<OpenInNewIcon fontSize='small' />}
+                  </a>
+                </td>
+                <td>Specialist</td>
+                <td>1517</td>
+                <td>2000+</td>
+              </tr>
+              <tr>
+                <td>Codechef</td>
+                <td>
+                  <a href="https://www.codechef.com/users/novaa" target='_blank' rel="noreferrer" style={{ all: "unset", color: "white" }}>
+                    <span style={{ color: "red" }}> novaa</span> {<OpenInNewIcon fontSize='small' />}
+                  </a>
+                </td>
+                <td>4 star</td>
+                <td>1788</td>
+                <td>200+</td>
+              </tr>
+              <tr>
+                <td>Leetcode</td>
+                <td>
+                  <a href="https://leetcode.com/novaa/" target='_blank' rel="noreferrer" style={{ all: "unset", color: "white" }}>
+                    <span style={{ color: "red" }}> novaa</span> {<OpenInNewIcon fontSize='small'/>}
+                  </a>
+                </td>
+                <td>Knight</td>
+                <td>1811</td>
+                <td>300+</td>
+              </tr>
+            </table>
+          </div>
+          <div id="skill2">
+            <h1> Development</h1>
+            <table>
+              <tr>
+                <th>Frontend</th>
+                <th>Backend</th>
+                <th>Languages</th>
+              </tr>
+              <tr>
+                <td>
+                  <span>React.js, Redux, HTML, CSS, Bootstrap, MaterialUI, Tailwind CSS</span>
+                </td>
+                <td>
+                  <span>
+                    Node.js, Express.js, Socket.io, Flask, MySQL, MongoDB
+                  </span>
+                </td>
+                <td>
+                  <span>
+                    JavaScript, C++, Python, TypeScript
+                  </span>
+                </td>
+              </tr>
+            </table>
+
+          </div>
         </div>
       </div>
     </div>
